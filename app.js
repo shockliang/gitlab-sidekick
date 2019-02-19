7703891a595a1fbe0ee35dd7f306f647bf253b9f
shocklaing <shockwater@gmail.com>
2019-02-19 22:14:10 +0800

    Adding error catch helpers.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require('path');
+const flash = require('connect-flash');
 const routes = require('./routes/index');
+const errorHandlers = require('./handlers/errorHandlers');
 
 // create our Express app
 const app = express();
@@ -11,8 +13,26 @@ app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work gr
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 app.use(express.static(path.join(__dirname, 'public')));
 
+// // The flash middleware let's us use req.flash('error', 'Shit!'), which will then pass that message to the next page the user requests
+app.use(flash());
+
 // After allllll that above middleware, we finally handle our own routes!
 app.use('/', routes);
+
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound);
+
+// One of our error handlers will see if these errors are just validation errors
+app.use(errorHandlers.flashValidationErrors);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors);
 
 // done! we export it so we can start the site in start.js
 module.exports = app;
