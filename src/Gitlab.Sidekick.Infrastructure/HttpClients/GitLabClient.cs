7e95efeb38b21bd2c8a93174bb428ea99de4af48
shockliang <shockwater@gmail.com>
2022-09-28 18:05:23 +0800

    Implementing search groups functionality.

--- src/Gitlab.Sidekick.Infrastructure/HttpClients/GitLabClient.cs
+++ src/Gitlab.Sidekick.Infrastructure/HttpClients/GitLabClient.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Gitlab.Sidekick.Application.Interfaces.HttpClients;
 using Gitlab.Sidekick.Application.Models.Groups;
 using RestSharp;
@@ -18,8 +19,21 @@ public class GitLabClient : IGitLabClient
     public async Task<Group> GetGroup(long groupId)
     {
         var request = CreateRequestWithToken($"groups/{groupId}", _token, Method.Get);
-
+        var response = await _client.GetAsync(request);
         return  await _client.GetAsync<Group>(request);
+    }
+
+    public async Task<ICollection<Group>> SearchGroups(string name)
+    {
+        var request = CreateRequestWithToken("groups", _token, Method.Get)
+            .AddParameter("search", name);
+
+        var response = await _client.GetAsync(request);
+
+        if (response.IsSuccessful)
+            return JsonSerializer.Deserialize<List<Group>>(response.Content);
+
+        return ArraySegment<Group>.Empty;
     }
 
     private static RestRequest CreateRequestWithToken(string resource, string token, Method method)
--- tests/Gitlab.Sidekick.Infrastructure.Tests/HttpClients/GitLabClientTests.cs
+++ tests/Gitlab.Sidekick.Infrastructure.Tests/HttpClients/GitLabClientTests.cs
@@ -12,17 +12,31 @@ public class GitLabClientTests
 {
     private readonly string _host = "http://localhost";
     private readonly string _token = "some token";
+    private readonly long _groupId = 1L;
+    private readonly string _searchName = "my_group";
+    private readonly IList<Group> _groupStubs;
     private readonly GitLabClient _target;
 
     public GitLabClientTests()
     {
         var groupStub = new Group { Id = 1, Description = "Group stub"};
         var groupJson = JsonSerializer.Serialize(groupStub);
+        _groupStubs = new List<Group>
+        {
+            new() { Id = 1, Name = $"{_searchName}_1", Description = "Group stub" },
+            new() { Id = 1, Name = $"{_searchName}_2", Description = "Group stub" }
+        };
+        var groupsJson = JsonSerializer.Serialize(_groupStubs);
+
         var mockHttp = new MockHttpMessageHandler();
 
         // Setup a respond for the user api (including a wildcard in the URL)
-        mockHttp.When($"{_host}/api/v4/groups/*")
+        mockHttp.When($"{_host}/api/v4/groups/{_groupId}")
             .Respond("application/json", groupJson);
+
+        mockHttp.When($"{_host}/api/v4/groups")
+            .WithQueryString("search", _searchName)
+            .Respond("application/json", groupsJson);
 
         // Instantiate the client normally, but replace the message handler
         var hostUri = new Uri(_host);
@@ -33,16 +47,61 @@ public class GitLabClientTests
         _target = new GitLabClient(client, _token);
     }
 
+    #region Get group tests
+
     [Fact]
     public async Task GetGroup_ShouldAsExpected()
     {
         // Arrange
 
         // Act
-        var actual = await _target.GetGroup(1);
+        var actual = await _target.GetGroup(_groupId);
 
         // Assert
         actual.Should().NotBeNull();
         actual.Id.Should().Be(1);
     }
+
+    [Fact]
+    public async Task ItShouldNotFound_WhenGetNotExistGroupId()
+    {
+        // Arrange
+
+        // Act
+        var actual = await _target.GetGroup(1024);
+
+        // Assert
+        actual.Should().BeNull();
+    }
+
+    #endregion
+
+    #region Search groups tests
+
+    [Fact]
+    public async Task ItShouldGetGroupList_WhenGetExistGroupName()
+    {
+        // Arrange
+
+        // Act
+        var actual = await _target.SearchGroups(_searchName);
+
+        // Assert
+        actual.Should().NotBeEmpty();
+        actual.Count.Should().Be(_groupStubs.Count);
+    }
+
+    [Fact]
+    public async Task ItShouldGet_EmptyCollection_WhenGetNotExistGroupName()
+    {
+        // Arrange
+
+        // Act
+        var actual = await _target.SearchGroups("not_exist_group");
+
+        // Assert
+        actual.Should().BeEmpty();
+    }
+
+    #endregion
 }
